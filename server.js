b505e8cd24d9d8146aa7f5318de1575c3f0b7d09
mannpatel20002 <manpatel20002@gmail.com>
2026-02-15 22:38:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.use(express.json());
 
 let location = { lat: 0, lng: 0 };
 
+app.get('/', (req, res) => {
+  res.send("GPS Tracker Backend Running ✅");
+});
+
 app.post('/update', (req, res) => {
   location = req.body;
   res.send("Location Updated");
