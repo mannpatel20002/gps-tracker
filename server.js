2b52db4e468edefaf58e8d1c82d59dd2243a7372
mannpatel20002 <manpatel20002@gmail.com>
2026-02-15 22:26:40 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let location = { lat: 0, lng: 0 };
+
+app.post('/update', (req, res) => {
+  location = req.body;
+  res.send("Location Updated");
+});
+
+app.get('/location', (req, res) => {
+  res.json(location);
+});
+
+app.listen(process.env.PORT || 3000);
